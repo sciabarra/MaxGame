da989627db92d3d9d952874c709a3ed6547ab2a1
Max <max@MicheleSciabarra.local>
2017-07-03 11:54:23 +0200

    aggiunto cubo verde

--- start.js
+++ start.js
@@ -6,18 +6,35 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 /* prepara un cubo e lo mette nella scena */
+// primo cubo
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var material = new THREE.MeshLambertMaterial({color: 0xf6546a})
 var cube = new THREE.Mesh( geometry, material );
+
+// secondo cubo
+var geometry1 = new THREE.BoxGeometry( 1.5,	1.5, 1.5 );
+var material1 = new THREE.MeshLambertMaterial({color: 0x00ff00})
+var cube1 = new THREE.Mesh( geometry1, material1 );
+cube1.position.x=4
+
+// luce
+var light = new THREE.PointLight(0xffffff, 1.2);
+
+// la scena
+light.position.set(0, 0, 6);
+scene.add( light );
 scene.add( cube );
+scene.add( cube1 )
 camera.position.z = 5;
+camera.position.y = 0;
 
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+  cube1.rotation.y += 0.02;
 	renderer.render(scene, camera);
 };
 animate();
+//renderer.render(scene, camera);

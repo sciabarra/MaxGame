3a215b3bdf3e8498255c41ef647942ee571b6cdb
Max <maxsciab@gmail.com>
2017-07-11 21:03:49 +0200

    drone otto direzioni

--- drone.js
+++ drone.js
@@ -11,7 +11,7 @@ loader.load('/drone.stl', function(geometry) {
   });
   drone = new THREE.Mesh(geometry, material);
   drone.position.set(0, 11, 0);
-  drone.rotation.z = Math.PI / 2
+  drone.rotation.z = -Math.PI / 2
   drone.rotation.x = Math.PI / 2
   drone.scale.set(0.5, 0.5, 0.5);
   drone.castShadow = true;
@@ -19,32 +19,44 @@ loader.load('/drone.stl', function(geometry) {
   scene.add(drone);
 })
 var pos = 0
-var modx = [0,-1,0,1]
-var modz = [1,0,-1,0]
+var modx = [0, 0.5, 1, 0.5, 0, -0.5, -1, -0.5]
+var modz = [-1, -0.5, 0, 0.5, 1, 0.5, 0, -0.5]
+
 document.onkeydown = function(e) {
   //console.log(e)
   //sinistra
-  if (e.keyCode == 37){
-    pos = Math.abs((pos - 1) % 4)
-    drone.rotation.z -= Math.PI / 2;
+  if (e.keyCode == 37) {
+    if(pos == 0) pos = 7
+    else pos = pos - 1
+    drone.rotation.z -= Math.PI / 4;
+    console.log("sinistra pos =" + pos)
+    console.log (modx[pos])
+    console.log (modz[pos])
   }
 
   //destra
-  if (e.keyCode == 39){
-    pos = (pos + 1) % 4
-    drone.rotation.z += Math.PI / 2;
+  if (e.keyCode == 39) {
+    pos = (pos + 1) % 8
+    drone.rotation.z += Math.PI / 4;
+    console.log("destra pos =" + pos)
+    console.log (modx[pos])
+    console.log (modz[pos])
   }
+
   //avanti
   if (e.keyCode == 38) {
     drone.position.z += modz[pos];
     drone.position.x += modx[pos];
   }
+
+
   //indietro
   if (e.keyCode == 40) {
     drone.position.z -= modz[pos];
     drone.position.x -= modx[pos];
   }
-  console.log (drone.position.x)
-  console.log (drone.position.z)
-  console.log ("pos ="+pos)
+
+
+  //console.log(drone.position.x)
+  //console.log(drone.position.z)
 }
